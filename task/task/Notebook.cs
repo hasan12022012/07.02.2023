fcb6c3ef7afbf67680a0930ef21e0ee5dd2059a3
Hasan Huseynli <hasansh@code.edu.az>
2023-02-06 20:29:57 +0400

    lastTask Added

---
+++ task/task/Notebook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace task
+{
+    internal class Notebook
+    {
+        public Notebook(string brand,string model)
+        {
+            Brand= brand;
+            Model= model;   
+        }
+
+        public string Brand;
+        public string Model;
+        public int Price;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} - Model: {Model} - Price: {Price}");
+        }
+
+       
+       
+    }
+}
---
+++ task/task/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook product1=new Notebook("asus", "FX506L");
+            product1.Price = 3000;
+            product1.ShowInfo();
+
+            Notebook product2 = new Notebook("apple", "MacBookPro");
+            product2.Price = 3500;
+            product2.ShowInfo();
+
+            Notebook product3 = new Notebook("hp", "pavilion");
+            product3.Price =2000 ;
+            product3.ShowInfo();
+
+
+            Notebook[] arr= new Notebook[] {product1, product2,product3};
+
+            var sum = 0;
+            var count=0;
+            for(int i=0; i<arr.Length; i++)
+            {
+                sum += arr[i].Price;
+                count++;
+            }
+            Console.WriteLine($"qiymetlerin ortalaması: {sum/count}");
+
+
+
+
+
+        }
+    }
+}
